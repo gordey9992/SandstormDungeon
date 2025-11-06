7229137d2daa8286cab42b819398f2e3590a4a39
gordey9992 <146545641+gordey9992@users.noreply.github.com>
2025-11-06 16:50:42 +0300

    Update МенеджерПесчанойБури.java

--- src/main/java/com/gordey25690/sandstormdungeon/менеджеры/МенеджерПесчанойБури.java
+++ src/main/java/com/gordey25690/sandstormdungeon/менеджеры/МенеджерПесчанойБури.java
@@ -3,6 +3,7 @@ package com.gordey25690.sandstormdungeon.менеджеры;
 import com.gordey25690.sandstormdungeon.ОсновнойПлагин;
 import com.gordey25690.sandstormdungeon.задачи.ЗадачаПесчанойБури;
 import org.bukkit.Location;
+import org.bukkit.block.Biome;
 import org.bukkit.scheduler.BukkitTask;
 
 /**
@@ -18,15 +19,30 @@ public class МенеджерПесчанойБури {
     }
     
     /**
-     * Запустить песчаную бурю в указанной локации
+     * Запустить песчаную бурю в указанной локации (только в пустыне)
      */
-    public void запуститьБурю(Location локация) {
+    public boolean запуститьБурю(Location локация) {
+        // Проверяем, что локация в пустыне
+        if (!этоПустыня(локация)) {
+            return false;
+        }
+        
         остановитьБурю(); // Останавливаем предыдущую бурю
         
         задачаБури = new ЗадачаПесчанойБури(плагин, локация).runTaskTimer(плагин, 0L, 5L);
         
-        // Отправляем сообщение в консоль
-        плагин.getLogger().info("Песчаная буря запущена в локации: " + локация);
+        плагин.getLogger().info("Песчаная буря запущена в пустыне: " + локация);
+        return true;
+    }
+    
+    /**
+     * Проверить, является ли биом пустыней
+     */
+    public boolean этоПустыня(Location локация) {
+        Biome биом = локация.getBlock().getBiome();
+        return биом == Biome.DESERT || 
+               биом == Biome.DESERT_HILLS || 
+               биом.toString().contains("DESERT");
     }
     
     /**
